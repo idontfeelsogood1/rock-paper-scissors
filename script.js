5f6131f8d4079cf4463bc3d0c385303728aaae58
Le Duong <leduongnewmemes123@gmail.com>
2025-05-02 23:11:29 +0700

    Finish initial structure of the project AND finish first function to get computer choice

---
+++ script.js
@@ -0,0 +1,15 @@
+// function that randomly returns “rock”, “paper” or “scissors”
+function getComputerChoice() {
+    let randomInt = Math.floor(Math.random() * 3);
+
+    if (randomInt === 0) {
+        return "rock";
+    } else if (randomInt === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+console.log(getComputerChoice());
+
+//

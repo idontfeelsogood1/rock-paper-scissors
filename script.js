87014359bdba027d0e27566a3206a7442570190b
Le Duong - Mac <leduongnewmemes123@gmail.com>
2025-05-05 19:16:39 +0700

    Finish base UI

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// function that randomly returns “rock”, “paper” or “scissors”
+// Function that randomly returns “rock”, “paper” or “scissors”
 function getComputerChoice() {
     let randomInt = Math.floor(Math.random() * 3);
 
@@ -11,12 +11,6 @@ function getComputerChoice() {
     }
 }
 
-// Write the logic to get the human choice
-function getHumanChoice() {
-    let userInput = prompt("Your selection:");
-    return userInput;
-}
-
 // Declare the players score variables
 let humanScore = 0;
 let computerScore = 0;
@@ -24,55 +18,72 @@ let computerScore = 0;
 // Write the logic to play a single round
 function playRound(humanChoice, computerChoice) {
     let underCasedHumanChoice = humanChoice.toLowerCase();
-
+    if (humanScore === 5 && computerScore === 5) {
+        document.querySelector(".score").innerText = `You tied!`;
+        return;
+    }
+    else if (humanScore === 5) {
+        document.querySelector(".score").innerText = `You won!`;
+        return;
+    }
+    else if (computerScore === 5) {
+        document.querySelector(".score").innerText = `You lost!`;
+        return;
+    };
+    
     if (underCasedHumanChoice === "rock" && computerChoice === "paper") {
         computerScore++;
-        console.log("You lose! Paper beats Rock");
     } else if (underCasedHumanChoice === "paper" && computerChoice === "rock") {
         humanScore++;
-        console.log("You win! Paper beats Rock");
     } 
 
     if (underCasedHumanChoice === "scissors" && computerChoice === "rock") {
         computerScore++;
-        console.log("You lose! Rock beats scissors");
     } else if (underCasedHumanChoice === "rock" && computerChoice === "scissors") {
         humanScore++;
-        console.log("You win! Rock beats scissors");
     }
 
     if (underCasedHumanChoice === "paper" && computerChoice === "scissors") {
         computerScore++;
-        console.log("You lose! Scissors beats paper");
     } else if (underCasedHumanChoice === "scissors" && computerChoice === "paper") {
         humanScore++;
-        console.log("You win! Scissors beats paper");
     }
 
-    if (
-        underCasedHumanChoice === "rock" && computerChoice === "rock"           || 
-        underCasedHumanChoice === "paper" && computerChoice === "paper"         ||
-        underCasedHumanChoice === "scissors" && computerChoice === "scissors") {
-        console.log("You tied!");
+    if (humanScore === 5 && computerScore === 5) {
+        document.querySelector(".score").innerText = `You tied!`;
+        return;
     }
+    else if (humanScore === 5) {
+        document.querySelector(".score").innerText = `You won!`;
+        return;
+    }
+    else if (computerScore === 5) {
+        document.querySelector(".score").innerText = `You lost!`;
+        return;
+    };
+
+    div = document.querySelector(".score");
+    div.innerText = `Your score: ${humanScore}
+                     Computer score: ${computerScore}`;
 
     return;
 }
 
-// Write the logic to play the entire game
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
+// Event listener for the buttons 
+buttonContainer = document.querySelector(".btnContainer")
+buttonContainer.addEventListener("click", (event) => {
+    let target = event.target;
+    switch(target.className) {
+        case 'rock':
+            playRound(document.querySelector(".rock").innerText, getComputerChoice())
+            break;
+        case 'paper':
+            playRound(document.querySelector(".paper").innerText, getComputerChoice())
+            break;
+        case 'scissors':
+            playRound(document.querySelector(".scissors").innerText, getComputerChoice())
+            break;;
     }
-    
-    if (humanScore > computerScore) {
-        console.log("You won the game!");
-    }
-    else if(computerScore > humanScore) {
-        console.log("You lose the game!");
-    }
-    else {
-        console.log("You tied the game!")
-    }
-}
-playGame();
+})
+
+
